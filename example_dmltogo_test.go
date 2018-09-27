4f7c6aaed889b5af75495ecb3a7f3ec88ca7e0eb
mnhkahn <lichao@cyeam.com>
2018-09-27 14:10:35 +0800

    add example for readme

--- example_dmltogo_test.go
+++ example_dmltogo_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/mnhkahn/dmltogo"
 )
 
-func ExampleNewLogger() {
+func Example() {
 	res, err := dmltogo.DmlToGo("CREATE TABLE `total_data` (`id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id', " +
 		"`region` varchar(32) NOT NULL COMMENT 'region name, like zh; th; kepler'," +
 		"`data_size` bigint NOT NULL DEFAULT '0' COMMENT 'data size;'," +
@@ -15,4 +15,35 @@ func ExampleNewLogger() {
 		"`comment` varchar(100) NOT NULL DEFAULT '' COMMENT 'comment'," +
 		"PRIMARY KEY (`id`))")
 	log.Println(res, err)
+	/*  res is:
+	package model
+
+	import (
+	    "database/sql"
+	    "time"
+
+	    "github.com/guregu/null"
+	)
+
+	var (
+	    _ = time.Second
+	    _ = sql.LevelDefault
+	    _ = null.Bool{}
+	)
+
+
+	type TotalDatum struct {
+	    ID int `gorm:"column:id;primary_key" json:"id"`
+	    Region string `gorm:"column:region" json:"region"`
+	    DataSize int64 `gorm:"column:data_size" json:"data_size"`
+	    Createtime time.Time `gorm:"column:createtime" json:"createtime"`
+	    Comment string `gorm:"column:comment" json:"comment"`
+
+	}
+
+	// TableName sets the insert table name for this struct type
+	func (t *TotalDatum) TableName() string {
+		return "TotalData"
+	}
+	*/
 }
